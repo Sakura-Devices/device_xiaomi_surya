67d35f376545a3bf11198fa3157962fabc2e5414
Petri Gynther <pgynther@google.com>
2018-04-17 18:55:35 -0700

    surya: A2DP offload: swap audio XML files
    Bug: 63932139
    Bug: 76201991
    Test: Manual A2DP testing (A2DP offload enabled and disabled)
    Signed-off-by: Unicote <arvogl874@gmail.com>
    Signed-off-by: agustkid <agustkid@gmail.com>

--- device.mk
+++ device.mk
@@ -92,7 +92,8 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
-    frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
+    frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
+    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
