ce2ae4b49b432d361d688fca6b8e451f09e809cd
Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>
2020-11-10 20:59:24 +0530

    surya: bringback blur props
    Signed-off-by: agustkid <agustkid@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -26,6 +26,13 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.bluetooth.disableinbandringing=false
 
+# Enable blurs
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.surface_flinger.supports_background_blur=1 \
+    ro.sf.blurs_are_expensive=1 \
+    debug.sf.latch_unsignaled=1 \
+    debug.sf.disable_backpressure=1
+
 # Graphics
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.hwui.renderer=skiavk \
