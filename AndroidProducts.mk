bb2820ece156a0bc32db49bb3da51bf4a2a22596
agustkid <agustkid@gmail.com>
2021-03-26 01:54:14 +0700

    surya: Wellcome to Sakura
    Change-Id: I622e8cca0e1a133ca83d9a42ba08838fed109cd3
    Signed-off-by: agustkid <agustkid@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_surya.mk
+    $(LOCAL_DIR)/lineage_surya.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_surya-user \
+    lineage_surya-userdebug \
+    lineage_surya-eng
--- lineage_surya.mk
+++ lineage_surya.mk
@@ -9,19 +9,21 @@ $(call inherit-product, device/xiaomi/surya/device.mk)
 # Inherit some
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Sakura flags
+SAKURA_OFFICIAL := true
+SAKURA_MAINTAINER := Agus&ArmSM
+TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_surya
+PRODUCT_NAME := lineage_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X3 NFC
 PRODUCT_MANUFACTURER := Xiaomi
-DEVICE_MAINTAINER := Ganesh Varma
 
-BUILD_FINGERPRINT := POCO/surya_global/surya:10/QKQ1.200512.002/V12.0.3.0.QJGMIXM:user/release-keys
-TARGET_INCLUDE_PIXEL_CHARGER := true
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
