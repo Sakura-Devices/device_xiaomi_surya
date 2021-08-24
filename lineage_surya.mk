0848e27256f09a5d43e04f7eb4ef61b9cf885683
agustkid <agustkid@gmail.com>
2021-08-24 08:49:22 +0700

    surya: wellcome to ProjectSakura
    - set config_deviceLightCapabilities to 232 for more light option
    - ship with basicgapps
    - enable blur by default
    
    Signed-off-by: agustkid <agustkid@gmail.com>

--- lineage_surya.mk
+++ lineage_surya.mk
@@ -24,6 +24,16 @@ $(call inherit-product, device/xiaomi/surya/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Sakura flags
+SAKURA_OFFICIAL := true
+SAKURA_MAINTAINER := Agus
+SAKURA_BUILD_TYPE=basicgapps
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+TARGET_USES_BLUR := true
+
+# Device identifier
 PRODUCT_NAME := lineage_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
