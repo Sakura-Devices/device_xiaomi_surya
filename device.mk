5f7c70fab56e98e741697c60e2c56316d2b984ae
Kuba Wojciechowski <nullbytepl@gmail.com>
2021-09-16 14:21:51 +0700

    surya: Ship FW with ROM
    * make it enforce https://gitlab.com/subhajeetmuhuri/proprietary_firmware_xiaomi_surya
    
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
    Signed-off-by: agustkid <agustkid@gmail.com>

--- device.mk
+++ device.mk
@@ -26,6 +26,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/surya/surya-vendor.mk)
 
+# Call Firmware setup
+$(call inherit-product, firmware/xiaomi/surya/Android.mk)
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
